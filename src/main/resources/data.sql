fd036edae545da247dd5ba7caf5ee8d164995e94
minah9999 <81949526+minah9999@users.noreply.github.com>
2023-03-12 00:45:45 +0900

    #10 - 테스트 데이터를 data.sql로 추가
    이걸로 테스트 통과함

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into article (title, content, created_by, modified_by, created_at, modified_at) values
+('title xxx', 'content xxx', 'kim', 'choi', '2021-05-30 23:53:46', '2021-03-10 08:48:50');
