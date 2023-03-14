d07a5402f636d373dde26a2a12967a66f39f8854
minah9999 <81949526+minah9999@users.noreply.github.com>
2023-03-14 15:36:40 +0900

    #20 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현

---
+++ src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
@@ -0,0 +1,69 @@
+package com.fastcampus.projectboard.service;
+
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.ArticleComment;
+import com.fastcampus.projectboard.dto.ArticleCommentDto;
+import com.fastcampus.projectboard.dto.ArticleDto;
+import com.fastcampus.projectboard.repository.ArticleCommentRepository;
+import com.fastcampus.projectboard.repository.ArticleRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@DisplayName("비즈니스 로직 - 댓글")
+@ExtendWith(MockitoExtension.class)
+class ArticleCommentServiceTest {
+
+    @InjectMocks
+    private ArticleCommentService sut;
+
+    @Mock
+    private ArticleCommentRepository articleCommentRepository;
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
+    @Test
+    void givenArticleId_whenSearchingComments_thenReturnsComments (){
+        //given
+        Long articleId = 1L;
+        BDDMockito.given(articleRepository.findById(articleId))
+                .willReturn(Optional.of(Article.of("title", "content", "#hashtag")));
+
+        //when
+        List<ArticleCommentDto> articleComments = sut.searchArticleComment(articleId);
+
+        //then
+        Assertions.assertThat(articleComments).isNotNull();
+        BDDMockito.then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
+    @Test
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment (){
+        //given
+        given(articleCommentRepository.save(any(ArticleComment.class)))
+                .willReturn(null);
+
+        //when
+        sut.saveArticleComment(ArticleCommentDto.of(LocalDateTime.now(), "test", LocalDateTime.now(), "test", "content"));
+
+        //then
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
+    }
+
+}
