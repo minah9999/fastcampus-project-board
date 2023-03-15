02a0b88461af6f507ef6729282e556b7b455e68c
minah9999 <81949526+minah9999@users.noreply.github.com>
2023-03-15 20:29:41 +0900

    #41 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을 모든 자바코드에 반영

--- src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -57,7 +57,7 @@ public class ArticleService {
 
 
     public void saveArticle(ArticleDto dto) {
-        UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().id());
+        UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
         articleRepository.save(dto.toEntity(userAccount));
     }
 
