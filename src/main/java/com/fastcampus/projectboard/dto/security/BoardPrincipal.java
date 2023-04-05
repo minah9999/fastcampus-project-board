04e0b4e7aa5e02537c652f37f8fde7c0ba572561
minah9999 <81949526+minah9999@users.noreply.github.com>
2023-04-05 17:28:01 +0900

    #70 - BoardPrincipal: OAuth 인증 정보를 다룰 수 있도록 기능 추가
    `OAuth2User` 인터페이스를 구현해서
    Spring OAuth Client 도 기존과 동일하게
    인증 정보를 인식할 수 있게 함.

--- src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
+++ src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,10 +18,15 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attribute
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return BoardPrincipal.of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attribute) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -28,7 +35,8 @@ public record BoardPrincipal(
                 roleTypes.stream().map(RoleType::getName).map(SimpleGrantedAuthority::new).collect(Collectors.toUnmodifiableSet()),
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attribute
         );
     }
 
@@ -60,6 +68,17 @@ public record BoardPrincipal(
     @Override public boolean isAccountNonLocked() {return true;}
     @Override public boolean isCredentialsNonExpired() {return true;}
     @Override public boolean isEnabled() {return true;}
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2Attribute;
+    }
+
+    @Override
+    public String getName() {
+        return username;
+    }
 
 
     public enum RoleType{
